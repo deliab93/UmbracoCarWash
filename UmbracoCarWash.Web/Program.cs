d16057eda3309d26f6c3335d0451e53db5519409
BDM <delia.burnei@nexergroup.com>
2025-02-19 14:09:28 +0000

    Added files

---
+++ UmbracoCarWash.Web/Program.cs
@@ -0,0 +1,33 @@
+using SixLabors.ImageSharp.Web.DependencyInjection;
+
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddDeliveryApi()
+    .AddComposers()
+    .Build();
+
+builder.Services.AddImageSharp();
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
